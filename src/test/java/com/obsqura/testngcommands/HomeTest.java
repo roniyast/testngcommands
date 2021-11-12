149ee479d62577872d92ad047c43875aa3d7fa7c
Roniya <roniyast@gmail.com>
2021-11-12 10:16:58 +0530

    testngcommands Commit 12_11

--- src/test/java/com/obsqura/testngcommands/HomeTest.java
+++ src/test/java/com/obsqura/testngcommands/HomeTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class HomeTest {
     @Test(priority = 1,enabled = true)
     public void verifyHomePageTitle(){
-        System.out.println("1");
+        System.out.println("Verifying Home Page Title");
     }
 
     @Test(priority = 2,enabled = true)
     public void verifyHomePageUserMenu(){
-        System.out.println("2");
+        System.out.println("Verifying Home Page User menu");
     }
 }
--- src/test/java/com/obsqura/testngcommands/LoginTest.java
+++ src/test/java/com/obsqura/testngcommands/LoginTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 public class LoginTest {
     @Test(priority = 3,enabled = true)
     public void verifyLoginPageTitle(){
-        System.out.println("3");
+        System.out.println("Verifying Login Page Title");
     }
 
     @Test(priority = 4,enabled = true)
     public void verifyValidLogin(){
-        System.out.println("4");
+        System.out.println("Verifying Login ");
 
     }
 }
